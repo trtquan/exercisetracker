27f01b3af6b4a70315a023c8d7e1f4871337905e
quantran <trtquan@gmail.com>
2020-09-21 19:48:43 +0700

    update

--- routes/api.js
+++ routes/api.js
@@ -1,102 +1,111 @@
-const Users = require('../models/users')
-const Exercises = require('../models/exercises')
+const Users = require("../models/users");
+const Exercises = require("../models/exercises");
 
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.post('/new-user', (req, res) => {
+router.post("/new-user", (req, res) => {
   const user = new Users(req.body);
   console.log(user);
-  user.save((err, savedUser) => { 
-    if (err) return console.log(err);;
+  user.save((err, savedUser) => {
+    if (err) return console.log(err);
     console.log(savedUser);
     res.json({
       username: savedUser.username,
-      _id: savedUser._id
-    })
-  })
-})
+      _id: savedUser._id,
+    });
+  });
+});
 
-router.get('/users', (req,res,next) => {
+router.get("/users", (req, res, next) => {
   Users.find({}, (err, data) => {
     if (err) return next(err);
-    res.json(data)
-  })
-})
+    res.json(data);
+  });
+});
 
-router.post('/add', (req, res, next) => {
+router.post("/add", (req, res, next) => {
   Users.findById(req.body.userId, (err, user) => {
     if (err) return next(err);
     if (!user) {
-      return next(err)
+      return next(err);
     }
-    if (req.body.date == '') {
+    if (req.body.date == "") {
       let today = new Date().toISOString().slice(0, 10);
       req.body.date = today;
     }
-    
-    const exercise = new Exercises(req.body)
-    exercise.save((err, savedExercise) => {
-      if (err) return next(err)
-      res.json(savedExercise)
-    })
-      
-  })
-})
 
-router.get('/log', (req, res, next) => {
-  if (Object.keys(req.query).length == 0 || req.query.userId == '') {
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+
+    const newExercise = new Exercise({
+      username,
+      description,
+      duration,
+      date,
+    });
+    exercise.save((err, savedExercise) => {
+      if (err) return next(err);
+      res.json(savedExercise);
+    });
+  });
+});
+
+router.get("/log", (req, res, next) => {
+  if (Object.keys(req.query).length == 0 || req.query.userId == "") {
     Exercises.find({}, (err, data) => {
       if (err) return next(err);
-      res.json(data)
-    })
+      res.json(data);
+    });
   } else {
-    console.log('from date: ' + req.query.from_date);
-    console.log('to date: ' + req.query.to_date);
-    const from = new Date(req.query.from_date)
-    const to = new Date(req.query.to_date)
-    console.log('from date: ' + from);
-    console.log('to date: ' + to);
-    console.log('non-empty /log request');
+    console.log("from date: " + req.query.from_date);
+    console.log("to date: " + req.query.to_date);
+    const from = new Date(req.query.from_date);
+    const to = new Date(req.query.to_date);
+    console.log("from date: " + from);
+    console.log("to date: " + to);
+    console.log("non-empty /log request");
     let out = {};
     Users.findById(req.query.userId, (err, user) => {
       Exercises.find({
         userId: req.query.userId,
         date: {
-          $lte: to != 'Invalid Date' ? to.getTime() : Date.now() ,
-          $gte: from != 'Invalid Date' ? from.getTime() : 0
-        }
+          $lte: to != "Invalid Date" ? to.getTime() : Date.now(),
+          $gte: from != "Invalid Date" ? from.getTime() : 0,
+        },
       })
-      .sort('-date')
-      .limit(parseInt(req.query.limit))
-      .exec((err, exercises) => {
-        if (err) return next(err)
-        if (exercises.length < 1) {
-          return next({status:400, message: 'unknown userId'})
-        } else {
-          console.log('user(typeof):  ' + user + '(' + typeof(user) + ')');
-          out = user.toJSON();
-          delete out.__v;
-          console.log('exercises.length:  ' + exercises.length);
-          out['count'] = exercises.length;
-          out['log'] = exercises.map(e => ({
-            description: e.description,
-            duration: e.duration,
-            date: e.date.toDateString()
-          }))
-          console.log(out);
-          res.json(out)
-        }
-      })
-    })
+        .sort("-date")
+        .limit(parseInt(req.query.limit))
+        .exec((err, exercises) => {
+          if (err) return next(err);
+          if (exercises.length < 1) {
+            return next({ status: 400, message: "unknown userId" });
+          } else {
+            console.log("user(typeof):  " + user + "(" + typeof user + ")");
+            out = user.toJSON();
+            delete out.__v;
+            console.log("exercises.length:  " + exercises.length);
+            out["count"] = exercises.length;
+            out["log"] = exercises.map((e) => ({
+              description: e.description,
+              duration: e.duration,
+              date: e.date.toDateString(),
+            }));
+            console.log(out);
+            res.json(out);
+          }
+        });
+    });
   }
-})
+});
 
-router.post('/delete-user', (req, res, next) => {
-  Exercises.deleteMany({userId: req.body.userId}, function(err){});
+router.post("/delete-user", (req, res, next) => {
+  Exercises.deleteMany({ userId: req.body.userId }, function (err) {});
   Users.findByIdAndRemove(req.body.userId, (err, user) => {
     if (err || !user) return next(err);
     res.send("Removed");
-  })
-})
+  });
+});
 
-module.exports = router
+module.exports = router;
