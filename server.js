08ffb258c5f7fad137a8ea1e58a0eb7bbd5ff656
quantran <trtquan@gmail.com>
2020-09-21 19:31:38 +0700

    update

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
+mongoose.Promise = global.Promise;
+
 mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology:true
