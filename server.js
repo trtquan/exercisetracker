7fb7014fde96dd1b9df193edd9fd6c72f2e34fb5
quantran <trtquan@gmail.com>
2020-09-21 15:12:14 +0700

    update connect db

--- server.js
+++ server.js
@@ -58,9 +58,11 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const shortid = require('shortid');
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI,{ useNewUrlParser: true })
-
-app.use(cors())
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology:true,
+});
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
